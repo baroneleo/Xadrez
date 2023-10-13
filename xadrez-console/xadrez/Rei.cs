2fb9a48ddc84532542dc623588367568341e547a
Leonardo Barone <leonardo.barone1996@gmail.com>
2023-10-13 17:21:02 -0300

    Implementacao das regras de movimento

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using tabuleiro;
+﻿using tabuleiro;
 
 namespace xadrez
 {
@@ -14,6 +9,93 @@ namespace xadrez
         public override string ToString()
         {
             return "R";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.Linhas, tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Acima
+
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Cima - Direita
+
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Direita
+
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Abaixo - Direita
+
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Abaixo
+
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Abaixo - Esquerda
+
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Esquerda
+
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+
+            // Acima - Esquerda
+
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            return mat;
         }
     }
 }
